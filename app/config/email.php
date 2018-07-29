d9e4771b143f2a626e8f7f0bd9baf9cd93ede182
Aleck Yann M <aleckyann@gmail.com>
2018-07-28 22:30:37 -0300

    Update email.php

--- app/config/email.php
+++ app/config/email.php
@@ -1,8 +1,8 @@
 <?php
 
-$config['smtp_host'] = 'smtp.umbler.com';
-$config['smtp_user'] = 'bot@wolko.com.br';
-$config['smtp_pass'] = '106.312.266-05wolko.com.br';
+$config['smtp_host'] = 'smtp.seu-smtp.com';
+$config['smtp_user'] = 'seu-email@seudominio.com';
+$config['smtp_pass'] = 'password-smtp';
 $config['smtp_port'] = 587;
 
 $config['protocol']  = 'smtp';
